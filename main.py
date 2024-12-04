d7ee1ce5f36b11c6d1e939601b5d90ffab89daa9
Pringles <pedroworkpartner@outlook.com>
2024-12-04 19:11:54 -0300

    commit

--- main.py
+++ main.py
@@ -2,36 +2,30 @@ import os
 from pydub import AudioSegment
 import speech_recognition as sr
 
-# Caminho para o arquivo de áudio
 audio_path = "coach.mp3"
-
 try:
-    # Carregar o áudio e recortar o primeiro minuto (60.000 ms)
+  
     audio = AudioSegment.from_mp3(audio_path)
     primeiro_minuto = audio[:60000]  # 60 segundos
 
-    # Exportar o áudio recortado como WAV
     wav_path = "primeiro_minuto.wav"
     primeiro_minuto.export(wav_path, format="wav", parameters=["-ar", "16000"])
     print("Primeiro minuto do áudio exportado com sucesso!")
 
-    # Inicializar o reconhecedor
     recognizer = sr.Recognizer()
 
-    # Abrir o áudio no formato WAV
     with sr.AudioFile(wav_path) as source:
         print("Carregando o áudio para reconhecimento...")
         audio_data = recognizer.record(source)
 
-    # Transcrever o áudio
     transcription = recognizer.recognize_google(audio_data, language="pt-BR")
     print("Transcrição bem-sucedida:", transcription)
 
-    # Salvar a transcrição em um arquivo de texto
     with open("transcricao.txt", "w", encoding="utf-8") as file:
         file.write(transcription)
     print("A transcrição foi salva em 'transcricao.txt'.")
-
+    
+#erros
 except FileNotFoundError:
     print("Erro: O arquivo de áudio não foi encontrado.")
 except sr.UnknownValueError:
